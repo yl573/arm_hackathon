5187771113f00ee9629d9f26a2279f26637575f0
tedeng <charlottehou1998@gmail.com>
2017-11-25 22:07:48 +0000

    integrated with fft

--- fft.py
+++ fft.py
@@ -7,48 +7,48 @@ Created on Sat Nov 25 16:26:37 2017
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy.fftpack import fft
 
 #y = x + 2*random.randrange(10);
 
 
-def fft(y,N=80,Ts=1.0/40.0):
+def fft_transform(y,N=80,Ts=1.0/40.0):
 
-    
-    from scipy.fftpack import fft
-    yf = fft(y)
-    xf = np.linspace(0.0, 1.0/(2.0*Ts), int(N/2))
-    yf_plt = 2.0/N * np.abs(yf[0:int(N/2)])
-    inte=np.sum(yf_plt[16:])
+	y_mag=[i[0]**2+i[1]**2+i[2]**2 for i in y]
+	yf = fft(y_mag)
+	xf = np.linspace(0.0, 1.0/(2.0*Ts), int(N/2))
+	yf_plt = 2.0/N * np.abs(yf[0:int(N/2)])
+	inte=np.sum(yf_plt[16:])
 
-    '''
-    from scipy.integrate import simps
-    inte = simps(yf_plt,xf)
-    print(inte)
-    '''
-    return xf,yf_plt,inte
-
-
-# Number of samplepoints
-N = 80
-# sample spacing
-Ts = 1.0 / 40.0
-t = np.linspace(0.0, N*Ts, N)
-y1 = 2*np.sin(6 * 2.0*np.pi*t) + 1*np.sin(10 * 2.0*np.pi*t)
-r = [random.randrange(1000)/1000 for i in y1]
-y = y1 #+ r
-y2 = [np.sin(6 * 2.0*np.pi*i) + 0.5*np.sin(10 * 2.0*np.pi*i) for i in t[40:]]
-y = [0]*40+y2
-xf, yf_plt, inte = fft(y)
+	'''
+	from scipy.integrate import simps
+	inte = simps(yf_plt,xf)
+	print(inte)
+	'''
+	return xf,yf_plt,inte
 
 
-print(inte)
-plt.plot(t, y)
-plt.grid()
-plt.show()
+# # Number of samplepoints
+# N = 80
+# # sample spacing
+# Ts = 1.0 / 40.0
+# t = np.linspace(0.0, N*Ts, N)
+# y1 = 2*np.sin(6 * 2.0*np.pi*t) + 1*np.sin(10 * 2.0*np.pi*t)
+# r = [random.randrange(1000)/1000 for i in y1]
+# y = y1 #+ r
+# y2 = [np.sin(6 * 2.0*np.pi*i) + 0.5*np.sin(10 * 2.0*np.pi*i) for i in t[40:]]
+# y = [0]*40+y2
+# xf, yf_plt, inte = fft(y)
 
-plt.plot(xf, yf_plt)
-plt.grid()
-plt.show()
+
+# print(inte)
+# plt.plot(t, y)
+# plt.grid()
+# plt.show()
+
+# plt.plot(xf, yf_plt)
+# plt.grid()
+# plt.show()
 
 '''
 # Learn about API authentication here: https://plot.ly/python/getting-started
--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import socket
 import numpy as np
+import fft
+import matplotlib.pyplot as plt
+import time
 
 app = Flask(__name__)
 
@@ -13,7 +16,21 @@ def hello_world():
 		readings_arr[i] = reading.split(' ')
 	float_readings = np.array(readings_arr).astype(np.float)
 
-	print(float_readings)
+	N = 40
+	Ts = 1.0/40
+	(xf,yf_plt,inte) = fft.fft_transform(float_readings,N=N,Ts=Ts)
+
+	
+	plt.xlabel('Frequency')
+	plt.ylabel('Power')
+	plt.ylim(0,1)
+	plt.plot(xf[2:], yf_plt[2:])
+	plt.pause(0.2)
+	plt.clf()
+	#plt.show(block=False)
+
+
+	print(yf_plt)
 	return 'ok'
 
 if __name__ == '__main__':

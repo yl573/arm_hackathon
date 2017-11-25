2e1c1100789a766fadb670fa8f4e2630c8aff3bd
ZhangShaojie1127 <33404725+ZhangShaojie1127@users.noreply.github.com>
2017-11-25 17:36:57 +0000

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,18 @@
+import time
+from sinchsms import SinchSMS
+
+number = '+447948050817'
+message = 'shaojie is smart'
+
+client = SinchSMS('0a0cb8f3-829b-4613-b34c-f96ed8a94978', 'ATOI/bqb0E2XJaSv1KOQ/Q==')
+
+print("Sending '%s' to %s" % (message, number))
+response = client.send_message(number, message)
+message_id = response['messageId']
+
+response = client.check_status(message_id)
+while response['status'] != 'Successful':
+    print(response['status'])
+    time.sleep(1)
+    response = client.check_status(message_id)
+print(response['status'])
